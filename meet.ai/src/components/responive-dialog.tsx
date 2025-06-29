44c86e22232fc16ae5df9ec3a594764818c8c410
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-06-29 23:04:10 +0530

    add:responive dialog box

---
+++ meet.ai/src/components/responive-dialog.tsx
@@ -0,0 +1,42 @@
+"use client"
+import { Drawer,DrawerContent,DrawerTitle,DrawerDescription, DrawerHeader } from "./ui/drawer"
+import { Dialog ,DialogContent,DialogHeader,DialogTitle,DialogDescription} from "./ui/dialog"
+import { useIsMobile } from "@/hooks/use-mobile"
+
+interface ResponseProps{
+    title:string;
+    description:string;
+    children:React.ReactNode;
+    open:boolean;
+    onOpenChange:(open:boolean)=>void
+}
+export  const ResponsiveDialog=({title,description,children, open,onOpenChange,}:ResponseProps)=>{
+ const isMoblie=useIsMobile()
+
+ if(isMoblie){
+    return(
+        <Drawer open={open} onOpenChange={onOpenChange}>
+         <DrawerContent>
+            <DrawerHeader>
+                <DrawerTitle>{title}</DrawerTitle>
+                <DrawerDescription>{description}</DrawerDescription>
+            </DrawerHeader>
+            <div className="p-4">
+               {children}
+            </div>
+         </DrawerContent>
+        </Drawer>
+    )
+}
+return(
+    <Dialog open={open} onOpenChange={onOpenChange}>
+     <DialogContent>
+        <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+          {children}
+     </DialogContent>
+    </Dialog>
+)
+}
